2fd3076f3087e16ef4d4b78cccf4b114b64de921
Rbustan0 <59246279+Rbustan0@users.noreply.github.com>
2023-03-31 10:02:38 -0700

    added some questions

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'projectTitle'
+    },
+    {
+        type: 'input',
+        message: 'Please provide a description for your project:',
+        name: 'bio'
+    },
+    {
+        type: 'input',
+        message: 'Provide examples and instructions for use of project:',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Provide examples and instructions for use of project:',
+        name: 'usage'
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
