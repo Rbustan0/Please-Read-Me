fb1c32a01e1a7c556e672216e39f4938aab35ca1
Rbustan0 <59246279+Rbustan0@users.noreply.github.com>
2023-03-31 14:11:52 -0700

    finished generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,17 +9,77 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license){
+    return '';
+  }
+  else return '\n* [License](#license)\n'
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(!license){
+    return '';
+  }
+  else return `## License
+  
+  This project uses the ${license} license!`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
 
-`;
+## Description 
+
+${data.bio}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+${renderLicenseLink(data.license)}
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+${data.collaborators}
+
+${data.thirdParty}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+To run tests, run the following command:
+
+\`\`\`
+${data.tests}
+\`\`\`
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at ${data.username}.`;
 }
 
 module.exports = generateMarkdown;
